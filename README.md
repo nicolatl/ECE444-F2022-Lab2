eb3e1f5b061bc79461da70d971e8845c37e3f97a
nicolatl <lawford.nicola@gmail.com>
2022-09-28 10:46:18 -0400

    Reproduce Example 2-2

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This repo is a clone of https://github.com/miguelgrinberg/flasky.
 
 Activity 1
 ![Alt text](/images/Activity1.png)
+
+Activity 2
+![Alt text](/images/Activity2.png)
--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def index(): # Index function shows a hello world HTML page
     return '<h1>Hello World!</h1>'
+
+@app.route('/user/<user_name>') # display custom hello message with user name on user page
+def user(user_name):
+    return '<h1>Hello, {}!</h1>'.format(user_name)
