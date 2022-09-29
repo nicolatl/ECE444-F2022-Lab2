5f0843e545b7ee27d2728a9dd553421ed907cdb0
nicolatl <lawford.nicola@gmail.com>
2022-09-28 21:46:12 -0400

    Complete Activity 4

--- README.md
+++ README.md
@@ -2,10 +2,23 @@ Nicola Lawford
 This repo is a clone of https://github.com/miguelgrinberg/flasky.
 
 Activity 1
+
 ![Alt text](/images/Activity1.png)
 
 Activity 2
+
 ![Alt text](/images/Activity2.png)
 
 Activity 3
+
 ![Alt text](/images/Activity3.png)
+
+Activity 4
+
+![Alt text](/images/Activity4-1.png)
+
+![Alt text](/images/Activity4-2.png)
+
+![Alt text](/images/Activity4-3.png)
+
+![Alt text](/images/Activity4-4.png)
--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class NameForm(FlaskForm): # define a name form
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your U of T email address?', validators=[Email()])
     submit = SubmitField('Submit')
 
 
@@ -17,13 +18,20 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
-def index(): # Index function shows a hello world HTML page
-    name = None
-    form = NameForm() # pass the name form defined earlier to the template
-    if form.validate_on_submit(): # check if valid data was submitted
-        name = form.name.data
-        form.name.data = '' # clear form field
-    return render_template('index.html', form=form, name=name)
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form, name = session.get('name'), email = session.get('email'))
+
 
 
 @app.route('/user/<user_name>') # display custom hello message with user name on user page
